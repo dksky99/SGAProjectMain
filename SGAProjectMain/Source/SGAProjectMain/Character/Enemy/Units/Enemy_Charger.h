4a8747ee74eeb55ca7baf9f1dc87b65ee5089f75
GoomDolRah <dksky9955@gmail.com>
2025-10-15 17:27:57 +0900

    animation fix

---
+++ SGAProjectMain/Source/SGAProjectMain/Character/Enemy/Units/Enemy_Charger.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../Enemy.h"
+#include "Enemy_Charger.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SGAPROJECTMAIN_API AEnemy_Charger : public AEnemy
+{
+	GENERATED_BODY()
+	
+};
---
+++ SGAProjectMain/Source/SGAProjectMain/Character/Enemy/Units/Enemy_Titan.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy_Spewer.h"
+#include "Enemy_Titan.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SGAPROJECTMAIN_API AEnemy_Titan : public AEnemy_Spewer
+{
+	GENERATED_BODY()
+	
+};
